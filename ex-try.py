4fef16ef55ffcbd0f6adf37028e7f3ad90e0bf84
Sibylau <jl3952@cornell.edu>
2020-12-14 18:24:06 -0500

    Add modulo to the exsitingsynthesizer

---
+++ ex-try.py
@@ -0,0 +1,112 @@
+import lark
+import z3
+import sys
+
+# A language based on a Lark example from:
+# https://github.com/lark-parser/lark/wiki/Examples
+GRAMMAR = """
+?start: sum
+  | sum "?" sum ":" sum -> if
+
+?sum: term
+  | sum "+" term        -> add
+  | sum "-" term        -> sub
+
+?term: item
+  | term "*"  item      -> mul
+  | term "/"  item      -> div
+  | term ">>" item      -> shr
+  | term "<<" item      -> shl
+  | term "%"  item      -> mod 
+
+?item: NUMBER           -> num
+  | "-" item            -> neg
+  | CNAME               -> var
+  | "(" start ")"
+
+%import common.NUMBER
+%import common.WS
+%import common.CNAME
+%ignore WS
+""".strip()
+
+
+def interp(tree, lookup):
+    """Evaluate the arithmetic expression.
+
+    Pass a tree as a Lark `Tree` object for the parsed expression. For
+    `lookup`, provide a function for mapping variable names to values.
+    """
+
+    op = tree.data
+    if op in ('add', 'sub', 'mul', 'div', 'shl', 'shr', 'mod'):  # Binary operators.
+        lhs = interp(tree.children[0], lookup)
+        rhs = interp(tree.children[1], lookup)
+        if op == 'add':
+            return lhs + rhs
+        elif op == 'sub':
+            return lhs - rhs
+        elif op == 'mul':
+            return lhs * rhs
+        elif op == 'div':
+            return lhs / rhs # floor division
+        elif op == 'shl':
+            return lhs << rhs
+        elif op == 'shr':
+            return lhs >> rhs
+        elif op == 'mod':
+            return lhs % rhs
+
+    elif op == 'neg':  # Negation.
+        sub = interp(tree.children[0], lookup)
+        return -sub
+    elif op == 'num':  # Literal number.
+        return int(tree.children[0])
+    elif op == 'var':  # Variable lookup.
+        return lookup(tree.children[0])
+    elif op == 'if':  # Conditional.
+        cond = interp(tree.children[0], lookup)
+        true = interp(tree.children[1], lookup)
+        false = interp(tree.children[2], lookup)
+        return (cond != 0) * true + (cond == 0) * false
+
+def solve(phi):
+    s = z3.Solver()
+    s.add(phi)
+    s.check()
+    return s.model()
+
+def z3_expr(tree, vars=None):
+    """Create a Z3 expression from a tree.
+
+    Return the Z3 expression and a dict mapping variable names to all
+    free variables occurring in the expression. All variables are
+    represented as BitVecs of width 8. Optionally, `vars` can be an
+    initial set of variables.
+    """
+
+    vars = dict(vars) if vars else {}
+
+    # Lazily construct a mapping from names to variables.
+    def get_var(name):
+        if name in vars:
+            return vars[name]
+        else:
+            v = z3.BitVec(name, 8)
+            vars[name] = v
+            return v
+
+    return interp(tree, get_var), vars
+
+if __name__ == '__main__':
+    parser = lark.Lark(GRAMMAR)
+    slow_prog, vars1 = z3_expr(parser.parse("x % 1"))
+    fast_prog, vars2 = z3_expr(parser.parse("h1? x: (x - h2 * x) "), vars1)
+
+    print(fast_prog)
+    plain_vars = {k: v for k, v in vars1.items()
+                    if not k.startswith('h')}
+
+    goal = z3.ForAll(list(plain_vars.values()), slow_prog == fast_prog)
+    
+    print(solve(goal))
